582a747a44aecb0c03313d96806ce58746aa5da1
jasachs <50554329+jasachs@users.noreply.github.com>
2026-01-29 08:41:35 -0800

    Add files via upload

--- generate_portfolio.py
+++ generate_portfolio.py
@@ -20,11 +20,11 @@ if "social_links" in data:
 # Set up Jinja environment
 env = Environment(loader=FileSystemLoader("."), autoescape=True)
 index_template = env.get_template("index_template.html")
-resume_template = env.get_template("resume_template.html")
+#resume_template = env.get_template("resume_template.html")
 
 # Render the template with the data
 html_output = index_template.render(**data)
-resume_output = resume_template.render(**data)
+#resume_output = resume_template.render(**data)
 
 # This is equivalent to...
 # html_output = index_template.render(name=data["name"], label=data["label"]...)
@@ -34,8 +34,8 @@ resume_output = resume_template.render(**data)
 with Path("index.html").open("w", encoding="utf-8") as f:
     f.write(html_output)
 
-with Path("resume.html").open("w", encoding="utf-8") as f:
-    f.write(resume_output)
+#with Path("resume.html").open("w", encoding="utf-8") as f:
+#    f.write(resume_output)
 
 print("HTML file generated successfully!")
 
